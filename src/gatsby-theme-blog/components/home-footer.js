f9f174000eef84e781f9e7921a98c243f71ac874
vpalmerini <victorpalmerini@gmail.com>
2020-05-24 17:23:55 -0300

    feat: add custom theme colors, bio and footer

---
+++ src/gatsby-theme-blog/components/home-footer.js
@@ -0,0 +1,38 @@
+import React, { Fragment } from "react";
+import { Styled, css } from "theme-ui";
+
+const Footer = () => {
+  const socialLinks = [
+    { name: "GitHub", url: "https://github.com/vpalmerini" },
+    { name: "LinkedIn", url: "https://www.linkedin.com/in/victorpalmerini/" },
+  ];
+
+  return (
+    <footer
+      css={css({
+        mt: 4,
+        pt: 3,
+      })}
+    >
+      {socialLinks
+        ? socialLinks.map((platform, i, arr) => (
+            <Fragment key={platform.url}>
+              <Styled.a
+                href={platform.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {platform.name}
+              </Styled.a>
+              {arr.length - 1 !== i && (
+                <Fragment>
+                  {` `}&bull;{` `}
+                </Fragment>
+              )}
+            </Fragment>
+          ))
+        : null}
+    </footer>
+  );
+};
+export default Footer;
